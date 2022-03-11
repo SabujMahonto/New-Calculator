fef3491f0ac626c753dfdae8db4b9009a2573ec4
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-03-11 21:04:10 +0600

    dinplay number show

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const previousOperand =document.querySelector('.previous_operand');
-const currentOperand = document.querySelector('.current_operand');
+const previousOperandElement =document.querySelector('.previous_operand');
+const currentOperandElement = document.querySelector('.current_operand');
 const acButton = document.querySelector('#ac');
 const delButton = document.querySelector('#del');
 const equalsButton = document.querySelector('#equals');
 const numberBtn = document.querySelectorAll('.number');
-const operatorBtn =document.querySelectorAll('.oprator');
+const operatorBtn =document.querySelectorAll('.operator');
+
+let previousOperand = ''
+let currentOperand = ''
+
+function updateDisplay(){
+currentOperandElement.innerHTML =currentOperand ;
+}
+
+function appendNumber(number){
+    currentOperand += number;
+    console.log(currentOperand)
+}
+
+numberBtn.forEach(btn=>{
+    btn.addEventListener('click',function(){
+        if(btn.textContent === '.' && currentOperand.includes('.')) return;
+        appendNumber(btn.textContent)
+        updateDisplay()
+    })
+})
