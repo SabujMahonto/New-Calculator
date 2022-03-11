70bd0a305ae9bb0382f1e616aee8705213968c00
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-03-11 19:53:57 +0600

    calculator functionlety start

---
+++ app.js
@@ -0,0 +1,7 @@
+const previousOperand =document.querySelector('.previous_operand');
+const currentOperand = document.querySelector('.current_operand');
+const acButton = document.querySelector('#ac');
+const delButton = document.querySelector('#del');
+const equalsButton = document.querySelector('#equals');
+const numberBtn = document.querySelectorAll('.number');
+const operatorBtn =document.querySelectorAll('.oprator');
